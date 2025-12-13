a3543668f964ad83313f0e36e41921da8106a82e
ThanhTam <traicute459@gmail.com>
2025-12-14 00:26:27 +0700

    fix loi mat css

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title : 'Quản Lý Kho Hàng'; ?></title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <div class="wrapper">
